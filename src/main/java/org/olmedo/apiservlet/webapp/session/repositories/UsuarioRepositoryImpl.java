74bb27c538b36a7bee9946b0376b965b53b12639
Damixn31 <damianol29@hotmail.com>
2023-02-09 20:32:23 +0100

    usuarios

--- src/main/java/org/olmedo/apiservlet/webapp/session/repositories/UsuarioRepositoryImpl.java
+++ src/main/java/org/olmedo/apiservlet/webapp/session/repositories/UsuarioRepositoryImpl.java
@@ -2,10 +2,8 @@ package org.olmedo.apiservlet.webapp.session.repositories;
 
 import org.olmedo.apiservlet.webapp.session.models.Usuario;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UsuarioRepositoryImpl implements UsuarioRepository{
@@ -19,22 +17,61 @@ public class UsuarioRepositoryImpl implements UsuarioRepository{
     //metodos
     @Override
     public List<Usuario> listar() throws SQLException {
-        return null;
+        List<Usuario> usuarios = new ArrayList<>();
+
+        try(Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from usuarios")) {
+            while(rs.next()) {
+                Usuario u = getUsuario(rs);
+                usuarios.add(u);
+            }
+        }
+        return usuarios;
     }
+
+
 
     @Override
     public Usuario porId(Long id) throws SQLException {
-        return null;
+        Usuario usuario = null;
+        try(PreparedStatement stmt = conn.prepareStatement("select * from usuarios where id=?")) {
+            stmt.setLong(1, id);
+            try(ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    usuario = getUsuario(rs);
+                }
+            }
+        }
+        return usuario;
     }
 
     @Override
     public void guardar(Usuario usuario) throws SQLException {
+        String sql;
+        if (usuario.getId() != null && usuario.getId() > 0) {
+            sql = "update usuarios set username=?, password=?, email=? where id=?";
+        } else {
+            sql = "insert into usuarios (username, password, email) values(?,?,?)";
+        }
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, usuario.getUsername());
+            stmt.setString(2, usuario.getPassword());
+            stmt.setString(3, usuario.getEmail());
 
+            if (usuario.getId() != null && usuario.getId() > 0) {
+                stmt.setLong(4, usuario.getId());
+            }
+            stmt.executeUpdate();
+        }
     }
 
     @Override
     public void eliminar(Long id) throws SQLException {
-
+        String sql = "delete from usuarios where id=?";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)){
+            stmt.setLong(1, id);
+            stmt.executeUpdate();
+        }
     }
 
     @Override
@@ -53,6 +90,15 @@ public class UsuarioRepositoryImpl implements UsuarioRepository{
                 }
             }
         }
+        return usuario;
+    }
+
+    private Usuario getUsuario(ResultSet rs) throws SQLException{
+        Usuario usuario = new Usuario();
+        usuario.setId(rs.getLong("id"));
+        usuario.setUsername(rs.getString("username"));
+        usuario.setPassword(rs.getString("password"));
+        usuario.setEmail(rs.getString("email"));
         return usuario;
     }
 }
--- src/main/java/org/olmedo/apiservlet/webapp/session/service/UsuarioServiceImpl.java
+++ src/main/java/org/olmedo/apiservlet/webapp/session/service/UsuarioServiceImpl.java
@@ -6,6 +6,7 @@ import org.olmedo.apiservlet.webapp.session.repositories.UsuarioRepositoryImpl;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Optional;
 
 public class UsuarioServiceImpl implements UsuarioService{
@@ -21,6 +22,43 @@ public class UsuarioServiceImpl implements UsuarioService{
             return Optional.ofNullable(usuarioRepository.porUsername(username)).filter(u -> u.getPassword().equals(password)); // el filter compara si el password es igual al de la base de datos
         } catch (SQLException throwables) {
             throw  new ServiceJdbcException(throwables.getMessage(), throwables.getCause());
+        }
+    }
+
+    @Override
+    public List<Usuario> listar() {
+        try {
+            return usuarioRepository.listar();
+        } catch (SQLException e) {
+            throw new ServiceJdbcException(e.getMessage(), e.getCause());
+        }
+    }
+
+    @Override
+    public Optional<Usuario> porId(Long id) {
+        try {
+          return Optional.ofNullable(usuarioRepository.porId(id));
+        } catch (SQLException e) {
+            throw new ServiceJdbcException(e.getMessage(), e.getCause());
+        }
+    }
+
+    @Override
+    public void guardar(Usuario usuario) {
+        try {
+            usuarioRepository.guardar(usuario);
+        } catch (SQLException e ) {
+            throw new ServiceJdbcException(e.getMessage(), e.getCause());
+        }
+
+    }
+
+    @Override
+    public void eliminar(Long id) {
+        try {
+            usuarioRepository.eliminar(id);
+        } catch (SQLException e) {
+            throw new ServiceJdbcException(e.getMessage(), e.getCause());
         }
     }
 }
